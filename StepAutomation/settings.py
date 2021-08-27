369a63ddd6ae2ca07106865a6bca928fc0a2600a
venkataprabhanjankumar <venkataprabhanjankumar@gmail.com>
2021-08-27 22:34:41 +0530

    commit

--- StepAutomation/settings.py
+++ StepAutomation/settings.py
@@ -30,7 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'StepAutomation.urls'
